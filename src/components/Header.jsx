5b6d58c3568b4c567467c7fea516ee970d7577c5
maheshchikkam <cmnaidu1206@gmail.com>
2025-08-15 13:33:40 +0530

    Removed all the mess.

--- src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="w-full py-4 px-6 bg-gray-100 border-b">
-      <h1 className="text-xl font-bold">Design Dazz App</h1>
-    </header>
-  );
-}
--- src/components/Main.jsx
+++
@@ -1,3 +0,0 @@
-export default function Main({ children }) {
-  return <main className="flex-1 p-6">{children}</main>;
-}
