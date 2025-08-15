ad73d7acc3b8c46c12d4a7e6ac4baf8dacacb4ba
maheshchikkam <cmnaidu1206@gmail.com>
2025-08-15 21:51:29 +0530

    Added header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import { NavLink } from "react-router-dom";
+import { FaAlignJustify } from "react-icons/fa";
+import { useState } from "react";
 
 import Logo from "./Logo";
 
 export default function Header() {
   return (
     <>
+      {/* Desktop Header */}
       <div className="hidden sm:h-[130px] sm:flex">
         <div className="h-full w-[30%] flex flex-row items-center justify-center bg-primary text-white p-4">
           <div>
@@ -50,14 +53,21 @@ export default function Header() {
           </NavLink>
         </div>
       </div>
-      <div className="flex h-[80px] xs:h-[80px] sm:hidden">
-        <div className="h-full w-[100%] flex flex-row items-center bg-primary text-white p-4">
-          <div>
-            <Logo />
+
+      {/* Mobile Header */}
+      <div className="flex h-[90px] xs:h-[80px] sm:hidden">
+        <div className="h-full w-[100%] flex flex-row items-center space-between bg-primary text-white p-4">
+          <div className="h-full w-[100%] flex flex-row items-center">
+            <div>
+              <Logo />
+            </div>
+            <div className="flex flex-col items-start ml-4 text-sm font-medium">
+              <span>DESIGN</span>
+              <span>DAZZ</span>
+            </div>
           </div>
-          <div className="flex flex-col items-start ml-4 text-sm font-medium">
-            <span>DESIGN</span>
-            <span>DAZZ</span>
+          <div>
+            <FaAlignJustify className="text-2xl text-white cursor-pointer" onClick={() => setMenuOpen(true)} />
           </div>
         </div>
       </div>
