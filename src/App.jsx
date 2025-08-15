5b8f76a4d6187e75d9c1cf0e943352837b744157
maheshchikkam <cmnaidu1206@gmail.com>
2025-08-15 21:03:10 +0530

    Added routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { useState } from "react";
 import Header from "./components/Header";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <Router>
       <div className="font-sans">
-        <Header></Header>
-        {/* <h1 className="text-3xl font-bold underline text-primary">
-          Hello world!
-        </h1> */}
+        <Header />
+        <div className="p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
       </div>
-    </>
+    </Router>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import Logo from "./Logo";
 
 export default function Header() {
@@ -13,7 +15,22 @@ export default function Header() {
         </div>
       </div>
       <div className="h-full w-[70%] flex flex-row items-center justify-center bg-secondary text-white p-4">
-        <div className="flex flex-col items-start justify-center ml-4 text-sm font-bold"></div>
+        <div className="flex flex-col items-start justify-center ml-4 text-sm font-bold">
+          <nav className="bg-gray-100 py-2 px-4 flex gap-4">
+            <Link to="/" className="hover:underline">
+              Home
+            </Link>
+            <Link to="/portfolio" className="hover:underline">
+              Portfolio
+            </Link>
+            <Link to="/about" className="hover:underline">
+              About
+            </Link>
+            <Link to="/contact" className="hover:underline">
+              Contact
+            </Link>
+          </nav>
+        </div>
       </div>
     </div>
   );
