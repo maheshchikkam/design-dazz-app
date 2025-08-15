c79c3de69fb179a1edb70b4988894f65d85fe388
maheshchikkam <cmnaidu1206@gmail.com>
2025-08-15 21:18:30 +0530

    Added Header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import Logo from "./Logo";
 
@@ -10,27 +10,43 @@ export default function Header() {
           <Logo />
         </div>
         <div className="flex flex-col items-start justify-center ml-4 text-sm font-medium">
-          <span>Design</span>
-          <span>Dazz</span>
+          <span>DESIGN</span>
+          <span>DAZZ</span>
         </div>
       </div>
-      <div className="h-full w-[70%] flex flex-row items-center justify-center bg-secondary text-white p-4">
-        <div className="flex flex-col items-start justify-center ml-4 text-sm font-bold">
-          <nav className="bg-gray-100 py-2 px-4 flex gap-4">
-            <Link to="/" className="hover:underline">
-              Home
-            </Link>
-            <Link to="/portfolio" className="hover:underline">
-              Portfolio
-            </Link>
-            <Link to="/about" className="hover:underline">
-              About
-            </Link>
-            <Link to="/contact" className="hover:underline">
-              Contact
-            </Link>
-          </nav>
-        </div>
+      <div className="h-full w-[70%] flex flex-row items-center justify-between bg-secondary font-normal px-[15%]">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "text-primary" : "text-primary-color"
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/portfolio"
+          className={({ isActive }) =>
+            isActive ? "text-primary" : "text-primary-color"
+          }
+        >
+          Portfolio
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "text-primary" : "text-primary-color"
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive ? "text-primary" : "text-primary-color"
+          }
+        >
+          Contact
+        </NavLink>
       </div>
     </div>
   );
