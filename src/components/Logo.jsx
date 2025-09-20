d191098006bab422825da08503ec21230b53874a
maheshchikkam <cmnaidu1206@gmail.com>
2025-09-20 16:40:00 +0530

    Added image lazy loading.

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,5 @@
 export default function Logo({ height = 45, width = 45 }) {
   return (
-    <img src="/favicon.svg" alt="Logo" height={height} width={width} style={{ display: 'block' }} />
+    <img src="/favicon.svg" alt="Logo" loading="lazy" height={height} width={width} style={{ display: 'block' }} />
   );
 }
